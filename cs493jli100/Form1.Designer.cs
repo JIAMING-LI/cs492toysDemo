7fa9141ba5ec1f63b80875528682965dc35e3800
Jiaming Li <jli100@illinois.edu>
2015-11-11 15:04:54 -0600

    s
    s

---
+++ cs493jli100/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace cs493jli100
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MedicationReconciliation = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MedicationReconciliation
+            // 
+            this.MedicationReconciliation.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MedicationReconciliation.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.MedicationReconciliation.Location = new System.Drawing.Point(0, 0);
+            this.MedicationReconciliation.Name = "MedicationReconciliation";
+            this.MedicationReconciliation.Size = new System.Drawing.Size(362, 109);
+            this.MedicationReconciliation.TabIndex = 0;
+            this.MedicationReconciliation.Text = "Medication-Reconciliation";
+            this.MedicationReconciliation.UseVisualStyleBackColor = true;
+            this.MedicationReconciliation.Click += new System.EventHandler(this.teamClick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button1.Location = new System.Drawing.Point(0, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 79);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Our team";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button2.Location = new System.Drawing.Point(196, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 79);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Milestones";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button3.Location = new System.Drawing.Point(12, 215);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(336, 93);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Business and Functional Requirements";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 320);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MedicationReconciliation);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.button1_Click_1);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button MedicationReconciliation;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ cs493jli100/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cs493jli100
+{
+    public partial class Form1 : Form
+    {
+        int memCount = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button wwr = (Button)sender;
+            wwr.Text = "Who we are?";
+        }
+
+        private void teamClick(object sender, EventArgs e)
+        {
+            Button wwr = (Button)sender;
+
+            Random randonGen = new Random();
+            Color randomColor = Color.FromArgb(randonGen.Next(255), randonGen.Next(255),
+            randonGen.Next(255));
+            wwr.ForeColor = randomColor;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Button member = (Button)sender;
+            String[] team_member = { "Diego Deveras", "Jiaming Li", "Muqi Zou", "Kelsey Yuen","Our Team" };
+            Random randonGen = new Random();
+            Color randomColor = Color.FromArgb(randonGen.Next(255), randonGen.Next(255),
+            randonGen.Next(255));
+            member.Text = team_member[memCount%5];
+            member.ForeColor = randomColor;
+            memCount++;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("1 .Develop an understanding of HIPAA compliant patient medication data – data structure\n"
+                + "2. Design a relational database table structure for both the client and server with focus on medication.\n"
+                + "3. Create a stub based Prescription Server\n"
+                + "4. Create a stub based client EMR proxy tool\n"
+                + "5. Develop Simple Exception Report based on mismatching medications\n"
+                + "6. Import / Export to clinical data architecture(CDA) / FHIR data format\n"
+                + "7. Reconcile 100 test patients\n"
+                + "8. Develop a sophisticated server communication mechanism – that mirrors medication ePrescribe\n"
+                + "9. Message Protoco\n"
+                + "10. Service\n"
+                + "11. Error Handling\n"
+                + "12. Develop client to reconcile medication data against real world provider\n"
+                + "13. Develop “production ready” Exception Reports”\n");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("·         By the end of the second semester, students are expected to deliver a functional data interface between medication ePrescribe and IMO’s EMR “Face Sheet\n"
+                    +"·         A functional version of the demonstration application should include the ability to have Medication content for 300 frequently prescribed drugs\n"
+                    +"·         Create an API that can handle one or many patient population medication reconciliations");
+        }
+    }
+}
